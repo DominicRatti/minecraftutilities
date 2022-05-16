5bdbdd2095e21a40355eb8a82ee2719f5838c2a7
DominicRatti <ratti.dominic@gmail.com>
2022-05-16 17:34:07 +0200

    added backpack system

---
+++ src/main/java/net/mcreator/minecraftutilities/procedures/LoadItemsProcedure.java
@@ -0,0 +1,74 @@
+package net.mcreator.minecraftutilities.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.minecraftutilities.MinecraftutilitiesModVariables;
+import net.mcreator.minecraftutilities.MinecraftutilitiesMod;
+
+import java.util.function.Supplier;
+import java.util.Map;
+
+public class LoadItemsProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MinecraftutilitiesMod.LOGGER.warn("Failed to load dependency world for procedure LoadItems!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MinecraftutilitiesMod.LOGGER.warn("Failed to load dependency entity for procedure LoadItems!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		Entity entity = (Entity) dependencies.get("entity");
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private IWorld world;
+
+			public void start(IWorld world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				if (entity instanceof PlayerEntity) {
+					Container _current = ((PlayerEntity) entity).openContainer;
+					if (_current instanceof Supplier) {
+						Object invobj = ((Supplier) _current).get();
+						if (invobj instanceof Map) {
+							ItemStack _setstack = ((entity.getCapability(MinecraftutilitiesModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+									.orElse(new MinecraftutilitiesModVariables.PlayerVariables())).itemSlot0);
+							_setstack.setCount((int) ((entity.getCapability(MinecraftutilitiesModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+									.orElse(new MinecraftutilitiesModVariables.PlayerVariables())).itemSlot0quantity));
+							((Slot) ((Map) invobj).get((int) (0))).putStack(_setstack);
+							_current.detectAndSendChanges();
+						}
+					}
+				}
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, (int) 1);
+	}
+}
---
+++ src/main/java/net/mcreator/minecraftutilities/procedures/SaveItemsProcedure.java
@@ -0,0 +1,103 @@
+package net.mcreator.minecraftutilities.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.minecraftutilities.MinecraftutilitiesModVariables;
+import net.mcreator.minecraftutilities.MinecraftutilitiesMod;
+
+import java.util.function.Supplier;
+import java.util.Map;
+
+public class SaveItemsProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MinecraftutilitiesMod.LOGGER.warn("Failed to load dependency world for procedure SaveItems!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MinecraftutilitiesMod.LOGGER.warn("Failed to load dependency entity for procedure SaveItems!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		Entity entity = (Entity) dependencies.get("entity");
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private IWorld world;
+
+			public void start(IWorld world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				{
+					ItemStack _setval = (new Object() {
+						public ItemStack getItemStack(int sltid) {
+							Entity _ent = entity;
+							if (_ent instanceof ServerPlayerEntity) {
+								Container _current = ((ServerPlayerEntity) _ent).openContainer;
+								if (_current instanceof Supplier) {
+									Object invobj = ((Supplier) _current).get();
+									if (invobj instanceof Map) {
+										return ((Slot) ((Map) invobj).get(sltid)).getStack();
+									}
+								}
+							}
+							return ItemStack.EMPTY;
+						}
+					}.getItemStack((int) (0)));
+					entity.getCapability(MinecraftutilitiesModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+						capability.itemSlot0 = _setval;
+						capability.syncPlayerVariables(entity);
+					});
+				}
+				{
+					double _setval = (new Object() {
+						public int getAmount(int sltid) {
+							if (entity instanceof ServerPlayerEntity) {
+								Container _current = ((ServerPlayerEntity) entity).openContainer;
+								if (_current instanceof Supplier) {
+									Object invobj = ((Supplier) _current).get();
+									if (invobj instanceof Map) {
+										ItemStack stack = ((Slot) ((Map) invobj).get(sltid)).getStack();;
+										if (stack != null)
+											return stack.getCount();
+									}
+								}
+							}
+							return 0;
+						}
+					}.getAmount((int) (0)));
+					entity.getCapability(MinecraftutilitiesModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+						capability.itemSlot0quantity = _setval;
+						capability.syncPlayerVariables(entity);
+					});
+				}
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, (int) 1);
+	}
+}
